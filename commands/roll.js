65b8d579833a498c487ce009dc089358313ec2e5
alucard87pl <marcin.jedrecki@gmail.com>
2021-03-08 18:52:23 +0100

    first implementation of rolling added simple predef cleanup waiting for https://github.com/GreenImp/rpg-dice-roller/issues/210#issue-824651721

--- commands/roll.js
+++ commands/roll.js
@@ -1,15 +1,22 @@
 const roller = require('rpg-dice-roller');
 // gave up, someone has this figured out:
 // repo: https://github.com/GreenImp/rpg-dice-roller
-// docs: 
+// docs: https://greenimp.github.io/rpg-dice-roller/
+
+const predefs = {
+    'AGE': '{2d6+d6}' //TODO: how to check for doubles
+}
 
 module.exports = async (msg, args) => {
-    let reply = 'Your Rolls:\n'
     if (!args.length) return;
-    args.forEach((r, index) => {
-        const roll = new roller.DiceRoll(r)
-        console.log(index, roll)
-        reply += 'Notation #' + (index + 1) + ': \`' + roll + `\`\n`
-    });
-    await msg.channel.send(`${reply}`);
+    args = args.toString().replace(',,', ','); //Roller() only takes one long String argument, not an Array;
+    console.log(args)
+    console.log("Predef:", args in predefs)
+    if (args in predefs) {
+        args = predefs[args]
+    }
+    const roll = new roller.DiceRoll(args)
+    console.log(roll)
+    //reply += 'Notation #' + (index + 1) + ': \`' + roll + `\`\n`
+    await msg.channel.send(`${roll}`);
 }
